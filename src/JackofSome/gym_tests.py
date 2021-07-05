b278dac51d54151c41ac6aa5dadfbd2b632abc97
Shreyans Jain <jshrey8@gmail.com>
2021-07-05 22:13:22 +0530

    pushing jack of some codes

---
+++ src/JackofSome/gym_tests.py
@@ -0,0 +1,15 @@
+import gym
+import time
+
+env = gym.make('CartPole-v0')
+observation = env.reset()
+
+for _ in range(1000):
+    env.render()
+    time.sleep(0.1)
+    import ipdb; ipdb.set_trace()
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    if done:
+        observation = env.reset()
+env.close()
